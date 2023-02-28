db913cccdd92fd08be377d590c4e44c91c407401
Alex <alex.wallerand@gmail.com>
2023-02-28 22:23:28 +0100

    fix

--- project/settings.py
+++ project/settings.py
@@ -98,16 +98,6 @@ CACHES = {
 }
 
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
-        }
-    }
-}
-
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
 

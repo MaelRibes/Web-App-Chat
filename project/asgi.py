86f932eb0738a17fe8530aa67a6aeaccd7973f2e
Alex <alex.wallerand@gmail.com>
2023-03-01 18:44:07 +0100

    add websocket for rooms

--- project/asgi.py
+++ project/asgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
+import room.routing
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebChat.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(room.routing.websocket_urlpatterns))
+        ),
+    }
+)
--- room/views.py
+++ room/views.py
@@ -7,27 +7,28 @@ from datetime import datetime
 (db_handle, client) = get_db_handle()
 db = client["WebChat"]
 
+
 def rooms(request):
     rooms = Room.objects.all()
     collection = db["Room"]
-    return render(request, 'room/rooms.html', {'rooms': collection.find() })
+    return render(request, 'room/rooms.html', {'rooms': collection.find()})
 
 
-def room(request,urlRoom):
+def room(request, urlRoom):
     collectionRoom = db["Room"]
     if request.method == 'POST':
         form = MessageForm(request.POST)
+        print(form)
         if form.is_valid():
             collectionMessages = db["Messages"]
             form = form.cleaned_data
             form["username"] = request.user.username
-            form["date"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            form["date"] = datetime.now().strftime("%-d/%-m/%Y %H:%M:%S")
             form["room"] = collectionRoom.find_one({"name": urlRoom})["_id"]
             collectionMessages.insert_one(form)
-            
+
     room = collectionRoom.find_one({"name": urlRoom})
     collectionMessages = db["Messages"]
-    messages = collectionMessages.find({"room" : room["_id"]})
+    messages = collectionMessages.find({"room": room["_id"]})
     form = MessageForm()
-    return render(request, 'room/room.html', {'room': room, "messages" : messages, 'form' : form})
-
+    return render(request, 'room/room.html', {'room': room, "messages": messages, 'form': form, "username": request.user.username})
